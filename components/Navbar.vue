814b9550022af74b3f385afb52cb6c4a24f6a6fe
Josh-moreton <josh@rwxt.org>
2024-12-08 18:59:31 +0000

    removed loads of stuff. Updated navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -32,53 +32,91 @@ onUnmounted(() => {
 })
 
 const drawer = ref(false)
+
+  import { shallowRef } from 'vue'
+
+
+  const items = [
+    {
+      text: 'Home',
+      to: '/'
+    },
+    {
+      text: 'Competitions',
+      to: '/competitions'
+    },
+    {
+      text: 'About',
+      to: '/about'
+    },
+    {
+      text: 'Contact',    
+      to: '/contact'
+
+    },
+  ]
 </script>
 
 <template>
   <v-layout>
-    <!-- Mobile Navigation Drawer -->
     <v-navigation-drawer v-model="drawer" color="primary" disable-resize-watcher>
       <v-list nav>
-        <v-list-item to="/" title="Home" />
-        <v-list-item to="/competitions" title="Competitions" />
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :active="i === 0"
+          :to="item.to"
+          :title="item.text"
+        />
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar class="px-4 px-md-8">
-      <!-- Mobile Menu Icon -->
+    <v-app-bar class="px-md-4">
       <template #prepend>
         <v-app-bar-nav-icon
           v-if="$vuetify.display.smAndDown"
           @click="drawer = !drawer"
-          class="me-2"
         />
       </template>
 
-      <!-- Logo -->
       <v-img
+        class="me-sm-8"
+        max-width="40"
         :src="logo"
-        alt="Victory Box"
-        max-height="80"
-        contain
-        class="me-4 me-sm-8"
-      />
+        />
 
-      <v-spacer />
-
-      <!-- Desktop Navigation -->
       <template v-if="$vuetify.display.mdAndUp">
-        <v-btn to="/" class="me-2 text-none" slim>Home</v-btn>
-        <v-btn to="/Competitions" class="me-2 text-none" slim>Competitions</v-btn>
-        <v-btn to="/Winners" class="me-2 text-none" slim>Winners</v-btn>
-        <v-btn to="/About" class="me-2 text-none" slim>About</v-btn>
-
+        <v-btn
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          :active="i === 0"
+          class="me-2 text-none"
+          slim
+          v-bind="item"
+        />
       </template>
 
       <v-spacer />
 
-
-      <!-- Theme and User Controls -->
       <template #append>
+        <v-btn
+          class="ms-1"
+          color="medium-emphasis"
+          icon="mdi-bell-outline"
+        />
+
+        <v-btn class="ms-1" icon>
+          <v-avatar image="https://cdn.vuetifyjs.com/images/john.png" />
+
+          <v-menu activator="parent" origin="top">
+            <v-list>
+              <v-list-item link title="Update profile" />
+
+              <v-list-item link title="Sign out" />
+            </v-list>
+          </v-menu>
+        </v-btn>
         <v-btn icon variant="text" @click="toggleTheme" class="ms-2"
           :text-color="theme.global.current.value.dark ? 'white' : 'black'">
           <PhMoon v-if="theme.global.current.value.dark" size="24" />
@@ -102,28 +140,12 @@ const drawer = ref(false)
       </template>
     </v-app-bar>
 
-    <v-main>
-      <slot></slot>
+    <v-main height="200">
+      <!--  -->
     </v-main>
   </v-layout>
 </template>
 
-<style scoped>
-.hidden {
-  display: none;
-}
 
-:deep(.v-btn) {
-  text-transform: none;
-  letter-spacing: 0;
-}
 
-.v-app-bar {
-  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
-}
 
-.v-img {
-  display: flex;
-  align-items: center;
-}
-</style>
